63b56a3ee6f5ca7675f67bbcb4237743cf1dc2c4
Jay <jaydeepdesai3332@gmail.com>
2022-09-10 15:09:36 +0530

    WalletappApplicationTests done

--- walletapp/src/main/java/com/spring/walletapp/repository/WalletRepository.java
+++ walletapp/src/main/java/com/spring/walletapp/repository/WalletRepository.java
@@ -2,6 +2,7 @@ package com.spring.walletapp.repository;
 
 import com.spring.walletapp.entity.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,6 @@ import java.util.List;
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet,Long> {
     List<Wallet> findAllByOrderByPriority();
-
+    @Query("Select case when count(s)>0 then true else false enf from Wallet w where w.id=?1")
+    Boolean isWalletExitsById(Long id);
 }
--- walletapp/src/test/java/com/spring/walletapp/WalletappApplicationTests.java
+++ walletapp/src/test/java/com/spring/walletapp/WalletappApplicationTests.java
@@ -1,13 +1,24 @@
 package com.spring.walletapp;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.factory.annotation.Autowired;
 
 @SpringBootTest
-class WalletappApplicationTests {
+public class WalletappApplicationTests {
 
+	@Autowired
+	private WalletappApplicationTests walletappApplicationTests;
 	@Test
-	void contextLoads() {
+	void isWalletExitsById(){
+		Wallet wallet = new Wallet(1,"Ram",1234);
+		wallet.save(wallet);
+		Boolean actualResult = wallet.isWalletExitsById(1);
+		assertThat(actualResult).isTrue();
 	}
+//	void contextLoads() {
+//	}
 
 }
